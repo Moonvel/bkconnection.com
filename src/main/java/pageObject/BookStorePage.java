c49f9e7487a3823d7fbf85bbf7d142ae4379da6d
Moonvel <moonvel@yandex.ru>
2024-07-19 11:31:58 +0300

    orderingSeveralDifferentBooksTest() implemented

--- src/main/java/pageObject/BookStorePage.java
+++ src/main/java/pageObject/BookStorePage.java
@@ -1,15 +1,18 @@
 package pageObject;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.$x;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
 public class BookStorePage {
     private final SelenideElement bookTitle = $("a.book-block-title");
     private final SelenideElement bookPrice = $("span.detail-page-price");
     private final SelenideElement addToCartOnBookTitlePage = $("div.detail-page-cart-button button.cart-btn");
-    private final SelenideElement addToCartOnBookStorePage = $("div.book-button-for-desktop.col-md-3 button");
+    public final SelenideElement addToCartOnBookStorePage = $("div.book-button-for-desktop.col-md-3 button");
+    private final ElementsCollection  addToCartElements = $$("div.book-button-for-desktop.col-md-3 button");
     private final SelenideElement continueShopping = $x("//a[text()='Continue Shopping']");
 
 
@@ -35,4 +38,9 @@ public class BookStorePage {
         continueShopping.click();
         return this;
     }
+
+    public ElementsCollection getAddToCartElements() {
+        return addToCartElements;
+    }
+
 }
